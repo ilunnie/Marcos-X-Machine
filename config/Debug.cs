42fa45ee3af3fa80cd5dda1e4ad2db116c67f96a
luizblank <luizrosacardoso@gmail.com>
2024-01-29 11:50:47 -0300

    Added cursor to game

--- config/Debug.cs
+++ config/Debug.cs
@@ -25,7 +25,7 @@ public static class Debug
         damagedbot.Entity = damagedBot;
 
         TileSets.tileSets();
-        TileSets.ReadFile("src/Area/FrenteEts.csv");
+        TileSets.DrawFromFile();
 
         
 
--- config/Game.cs
+++ config/Game.cs
@@ -7,7 +7,7 @@ public class Game : App
     public ILevel Level { get; set; }
     public override void Open()
     {
-    
+        this.form.Cursor = new Cursor("src/sprites/cursor.cur");
 
         // for (int i = 0; i < 1000; i++)
         // {
@@ -18,10 +18,10 @@ public class Game : App
     }
     public override void OnFrame()
     {
-        if (Level.LoadPercent != 100)
-        {
-            Level.Load();
-            return;
-        }
+        // if (Level.LoadPercent != 100)
+        // {
+        //     Level.Load();
+        //     return;
+        // }
     }
 }
